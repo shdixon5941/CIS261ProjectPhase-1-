ab6cd986235368d8d4734abbffb8b833c441afff
strayer_practice@outlook.com <strayer_practice@outlook.com>
2023-01-18 21:19:15 -0800

    Add project files.

---
+++ CIS261ProjectPhase1.py
@@ -0,0 +1,68 @@
+#
+#
+#
+def GetEmpName():
+    empname = input("Enter employee name: ")
+    return empname
+#for the next three functions, you need to convert the input to a float, e.g., varname = float(input('descripion of input:  '))
+#write the GetHoursWorked function
+
+
+#write the GetHourlyRate function
+
+
+# write the GetTaxRate function
+
+
+
+
+def CalcTaxAndNetPay(hours, hourlyrate, taxrate):
+    grosspay = hours * hourlyrate
+    incometax = grosspay * taxrate
+    netpay = grosspay - incometax
+    return grosspay, incometax, netpay
+
+def printinfo(empname, hours, hourlyrate,grosspay, taxrate, incometax, netpay):
+    print("Name:  ", empname) 
+    print("Hours Worked: ", f"{hours:,.2f}")
+    # write the lines of code to display hourlyrate, grosspay, taxrate, incometax and netpay with correct formatting
+    # taxrate needs to be formatted as percentage
+
+
+
+
+
+    print()
+def PrintTotals(TotEmployees, TotHours, TotGrossPay, TotTax, TotNetPay):    
+    print()
+    print(f"Total Number Of Employees: {TotEmployees}")
+    print(f"Total Hours Worked: {TotHours:,.2f}")
+    # write the code to print  TotGrossPay, TotTax, and TotNetpay with 2 decimal places
+
+
+if __name__ == "__main__":
+    TotEmployees = 0
+    TotHours = 0.00
+    TotGrossPay = 0.00
+    TotTax = 0.00
+    TotNetPay = 0.00
+    while True:
+        empname = GetEmpName()
+        if (empname.upper() == "END"):
+            break
+        # write the code to assign to hours the return value from GetHoursWorked
+
+        # write the code to assign to hourlyrate the return value from GetHourlyRate
+
+        # write the code to assign to taxrate the return value from GetTaxRate
+        
+        grosspay, incometax, netpay = CalcTaxAndNetPay(hours, hourlyrate, taxrate)
+        printinfo(empname, hours, hourlyrate, grosspay, taxrate, incometax, netpay)
+        TotEmployees += 1
+        TotHours += hours
+        # write the code to increment the other total variables with the appropriate values
+
+
+
+    PrintTotals (TotEmployees, TotHours, TotGrossPay, TotTax, TotNetPay)
+
